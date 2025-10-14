9e2d5d8a1e8bac9286f219c6efdc2fc584661213
Alexandru <Burada Alexandru>
2025-10-14 15:21:42 +0300

    set-up app.properties file to configure conection with MySQL db

--- src/main/java/com/AlexBurada/Barbershop/BarbershopApplication.java
+++ src/main/java/com/AlexBurada/Barbershop/BarbershopApplication.java
@@ -2,12 +2,15 @@ package com.AlexBurada.Barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BarbershopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BarbershopApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(BarbershopApplication.class, args);
+
 	}
 
 }
